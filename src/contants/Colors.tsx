4da80d98b10e75e34e2b2e70b0d5bed8abeb0e50
Hamam Nasrodin <hamam.nasrodin@maybank.com>
2024-07-26 22:34:03 +0700

    add new color

---
+++ src/contants/Colors.tsx
@@ -0,0 +1,11 @@
+const Colors = {
+  blue_1: '#173B89',
+  blue_2: '#0DABE2',
+  white: 'white',
+  gray_1: '#E8E8E8',
+  gray_2: '#7C7C7C',
+  orange_1: '#E68933',
+  orange_2: '#FFBE81',
+}
+
+export default Colors
