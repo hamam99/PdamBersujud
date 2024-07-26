2ace7f7d282e6294803afab6dbafa0cb239c283b
Hamam Nasrodin <hamam.nasrodin@maybank.com>
2024-07-26 22:34:16 +0700

    update

--- src/navigation/RootNavigation.tsx
+++ src/navigation/RootNavigation.tsx
@@ -1,20 +1,27 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Home from '../pages/Home'
 import Splash from '../pages/Splash'
+import { RouteName } from './RouteName'
+import Login from '../pages/Login'
 
 const Stack = createNativeStackNavigator()
 
 const RootNavigation = () => {
   return (
-    <Stack.Navigator initialRouteName="Splash">
+    <Stack.Navigator initialRouteName={RouteName.SPLASH}>
       <Stack.Screen
-        name="Splash"
+        name={RouteName.SPLASH}
         component={Splash}
         options={{ headerShown: false }}
       />
       <Stack.Screen
-        name="Home"
+        name={RouteName.HOME}
         component={Home}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name={RouteName.LOGIN}
+        component={Login}
         options={{ headerShown: false }}
       />
     </Stack.Navigator>
--- src/navigation/RootStackParamList.tsx
+++ src/navigation/RootStackParamList.tsx
@@ -3,4 +3,5 @@ import { RouteName } from './RouteName'
 export type RootStackParamList = {
   [RouteName.SPLASH]: undefined
   [RouteName.HOME]: undefined
+  [RouteName.LOGIN]: undefined
 }
