fc9eab2083a78928650f5de4fa49c8efd6cfa2dd
Hamam Nasrodin <hamam.nasrodin@maybank.com>
2024-07-28 01:05:11 +0700

    update

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React from 'react'
+import { Text, View } from 'react-native'
+import Account from '../../components/local/Home/Account'
 
 const Home = () => {
   return (
-    <View>
+    <View
+      style={{
+        flex: 1,
+      }}
+    >
+      <Account />
       <Text>HOME</Text>
     </View>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
