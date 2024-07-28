7dbbfd6fa25f857a6b45610b397c3e9827f91978
Hamam Nasrodin <hamam.nasrodin@maybank.com>
2024-07-28 12:47:40 +0700

    add props left icon

--- src/components/global/Button/index.tsx
+++ src/components/global/Button/index.tsx
@@ -9,23 +9,64 @@ import {
 import Colors from '../../../contants/Colors'
 import Fonts from '../../../contants/Fonts'
 
+type IMode = 'outline' | 'contain'
+
 type ButtonProps = {
   label?: string
   onPress?: () => void
   containerStyle?: StyleProp<ViewStyle> | undefined
   labelStyle?: StyleProp<TextStyle> | undefined
+  mode?: IMode
+  LeftIcon?: React.JSX.Element
 }
 
 const Button = (props: ButtonProps) => {
-  const { label, onPress = () => null, containerStyle, labelStyle } = props
+  const {
+    label,
+    onPress = () => null,
+    containerStyle,
+    labelStyle,
+    mode = 'contain',
+    LeftIcon,
+  } = props
+
+  const currentMode = ListMode[mode]
+
   return (
-    <TouchableOpacity style={[styles.container, containerStyle]}>
-      <Text style={[styles.label, labelStyle]}>{label}</Text>
+    <TouchableOpacity
+      style={[styles.container, currentMode?.container, containerStyle]}
+      onPress={onPress}
+    >
+      {LeftIcon && LeftIcon}
+      <Text style={[styles.label, currentMode?.label, labelStyle]}>
+        {label}
+      </Text>
     </TouchableOpacity>
   )
 }
 
 export default Button
+
+const ListMode = {
+  outline: {
+    container: {
+      backgroundColor: 'white',
+      borderColor: Colors.blue_1,
+      borderWidth: 1,
+    },
+    label: {
+      color: Colors.blue_1,
+    },
+  },
+  contain: {
+    container: {
+      backgroundColor: Colors.blue_1,
+    },
+    label: {
+      color: Colors.white,
+    },
+  },
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -35,6 +76,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: Colors.blue_1,
     borderRadius: 10,
+    flexDirection: 'row',
+    gap: 4,
   },
   label: {
     fontFamily: Fonts.PoppinsRegular,
