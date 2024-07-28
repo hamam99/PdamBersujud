6853f45fd156eadb2dccb14b6670f07da4083d24
Hamam Nasrodin <hamam.nasrodin@maybank.com>
2024-07-29 00:33:06 +0700

    update

---
+++ src/components/global/index.tsx
@@ -0,0 +1,7 @@
+import Button from './Button'
+import IconTabBar from './IconTabBar'
+import HeaderBar from './HeaderBar'
+import ShowToast from './ShowToast'
+import TextInputCustom from './TextInputCustom'
+
+export { Button, IconTabBar, HeaderBar, ShowToast, TextInputCustom }
